ebb31c476227a90d0a68f2326cd668e53438ea79
AskurTomasson <114407354+AskurTomasson@users.noreply.github.com>
2023-09-27 22:31:14 +0200

    finalize
    finalize

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ let imdbIdArray = []
 
 searchBtn.addEventListener('click', getSearchedMovies)
 
+
+
 function getSearchedMovies() {
+
+    // GET ALL SEARCH RESPONSES FROM INPUT VALUE //
     fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput.value}`)
     .then(resp => resp.json())
     .then(data => {
-        
         if(data.Search) {
+            // PUSHING ALL UNIQUE SEARCH REPONSES IDS TO ARRAY //
             for(let ids of data.Search) {
                 imdbIdArray.push(ids.imdbID)
             }
@@ -22,18 +26,60 @@ function getSearchedMovies() {
         }
         
     })
+
+    // LOOPING OVER imdbIdArray TO SEND REQUEST ON EACH UNIQUE ID //
     .then(() => {
         for(let id of imdbIdArray) {
             fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
             .then(resp => resp.json())
             .then(data => {
-                console.log(data)
+                imdbIdArray = []
+
+                if (data.Rated != "N/A" && data.Runtime != "N/A") {
+                // GIVE WATCHLIST BUTTON THE CONNECTED IDS OF THE RESPONSES //
+                document.addEventListener('click', (e) => {
+                    if(e.target.dataset.watchlistbtn === `${data.imdbID}`) {
+                        addToWatchList(e.target.dataset.watchlistbtn)
+                        document.getElementById(`${data.imdbID}`).innerHTML = "added"
+                    }
+                })
+
                 renderMovie.innerHTML += `
                     <div class="card">
-                    <p>${data.Title}</p>
-                    </div>`
-                    // CONTINUE RENDERING THE REST OF THE DATA //
+                        <img class="data-poster" src="${data.Poster}">
+                        <div class="card-info">
+                            <div class="top-card-div">  
+                                <h3 class="data-title">${data.Title}</h3>
+                                <p class="data-rating">⭐${data.imdbRating}</p>
+                            </div>
+                            <div class="mid-card-div">
+                                <p class="data-runtime">${data.Runtime}</p>
+                                <p class="data-genre">${data.Genre}</p>
+                                <button id="${data.imdbID}" data-watchlistbtn="${data.imdbID}" class="data-btn">
+                                <i class="fa fa-light fa-circle-plus"></i>watchlist</button>
+                            </div>
+                            <p class="data-plot">${data.Plot}</p>
+                        </div>
+                    </div>
+                    <hr>`
+                }
             })
         }
     })
+
+    // CLEAR INPUT AND RENDER //
+    searchInput.value = ""
+    renderMovie.innerHTML = ""
+}
+
+// THE ID OF THE MOVIE TO ADD IS GIVEN TO US/CONNECTED WITH idOfMovie //
+// BY CALLING THE FUNCTION WITH THE DATASET SET TO EACH UNIQUE ID OF RESPONSE //
+const watchlist = JSON.parse(localStorage.getItem('watchlist')) || []
+
+function addToWatchList(idOfMovie) {
+    if(!watchlist.includes(idOfMovie)) {
+        watchlist.push(idOfMovie)
+        localStorage.setItem(`watchlist`,JSON.stringify(watchlist))
+        console.log(`${watchlist}`)
+    }
 }
---
+++ watchlist.js
@@ -0,0 +1,47 @@
+const placeholderTitle = document.getElementById('placeholder-title')
+const watchlistContainer = document.getElementById('movie-render') 
+const apiKey = "5e8f8921"
+const watchlist = JSON.parse(localStorage.getItem("watchlist")) || []
+
+// FETCHING MOVIES BY ID AND RENDERING THEM TO PAGE //
+for(movieId of watchlist) {
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`)
+        .then(res => res.json())
+        .then(data => {
+            placeholderTitle.style.display = "none"
+
+            document.addEventListener('click', (e) => {
+                if(e.target.dataset.watchlistbtn === `${data.imdbID}`) {
+                    removeFromWatchlist(e.target.dataset.watchlistbtn)
+                }
+            })
+                
+            watchlistContainer.innerHTML += `
+                    <div class="card">
+                        <img class="data-poster" src="${data.Poster}">
+                        <div class="card-info">
+                            <div class="top-card-div">  
+                                <h3 class="data-title">${data.Title}</h3>
+                                <p class="data-rating">⭐${data.imdbRating}</p>
+                            </div>
+                            <div class="mid-card-div">
+                                <p class="data-runtime">${data.Runtime}</p>
+                                <p class="data-genre">${data.Genre}</p>
+                                <button data-watchlistbtn="${data.imdbID}" class="data-btn">
+                                <i class="fa fa-light fa-circle-minus"></i>remove</button>
+                            </div>
+                            <p class="data-plot">${data.Plot}</p>
+                        </div>
+                    </div>
+                    <hr>`
+        })
+}
+
+
+function removeFromWatchlist(movieId) {
+    if(watchlist.includes(movieId)) {
+        watchlist.splice(watchlist.indexOf(`${movieId}`), 1)
+        localStorage.setItem("watchlist", JSON.stringify(watchlist))
+        location.reload()
+    }
+}
